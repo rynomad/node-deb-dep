14ff2faa499caf76a6278288620e1fd60d4c106b
Ryan Bennett <ryan@filament.com>
2016-03-03 19:02:13 -0700

    cleanup

--- mounter.sh
+++ mounter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-f=./testapp/mount.txt
-#for file in $FILES
-#do
+FILES=/etc/node-deb-dep/mounts/
+for file in $FILES
+do
   echo "setting mounts for $file file..."
   # take action on each file. $f store current file name
   while read src target ; do 
@@ -9,4 +9,4 @@ f=./testapp/mount.txt
     /bin/mount --bind --make-slave $src $target
     /bin/mount -o remount,ro,bind $src $target
   done < $f
-#done
+done
